545954b34c1185269ca7204188ffe939c7286d92
charmacist <charmacist@DESKTOP-6HHSHH.(none)>
2023-03-22 17:01:56 +0900

    03221

--- uk_ecommerce/고객새그먼트.sql
+++ uk_ecommerce/고객새그먼트.sql
@@ -3,3 +3,55 @@
 -- #frequency: 어느 정도로 자주 구입했는지?
 -- #Monetary: 구입한 총 금액
 
+--#고객의 마지막 구매일 출력
+select CustomerId,
+max(InvoiceDate) mndt
+from clothes.dataset3 
+group by 1;  -- 2011년 12월 2일
+
+--#가장 최근 날짜로부터 TIMER INTERVAL시간 차이 계산
+-- ##1. RECENCY (얼마나 최근에 구매했는지)
+select CustomerID,
+datediff('2011-12-02', mxdt) recency
+from
+(select CustomerID,
+max(InvoiceDate) mxdt 
+from clothes.dataset3 
+group by 1) a;
+
+-- #재구매 segment
+-- #고객별, 상품별, 구매연도 카운트
+select CustomerID, StockCode, 
+count(distinct substr(InvoiceDate,1,4)) unique_yy
+from clothes.dataset3 
+group by 1,2;
+
+-- ## unique_yy가 2 이상인 고객 : 2년에 걸쳐 구매 (재구매 고객)
+select CustomerId,
+max(unique_yy) mx_unique_yy
+from
+(select CustomerID,
+StockCode,
+count(distinct substr(InvoiceDate,1,4)) unique_yy
+from clothes.dataset3
+group by 1,2
+)a
+group by 1;
+
+-- unique_yy가 2 이상인 고객과 그렇지 않은 고객 구분
+select CustomerID,
+case when mx_unique_yy >= 2 then 1 else 0 end repurchase_segment
+from
+(select CustomerId,
+max(unique_yy) mx_unique_yy
+from
+(select CustomerID,
+StockCode,
+count(distinct substr(InvoiceDate,1,4)) unique_yy
+from clothes.dataset3
+group by 1,2
+)a
+group by 1) a
+group by 1;
+
+
--- uk_ecommerce/코호트분석.sql
+++ uk_ecommerce/코호트분석.sql
@@ -1,4 +1,44 @@
 -- 첫 구매 월, 가입 월, 구매 월 기준으로 시간의 흐름에 따른 변화
 -- 1) 첫 구매월 기준으로 그룹 간의 패턴 분석
--- # 고객별 첫 구매일 구함
--- # 각 고객의 주문 일지, 구매액 조회
+-- # 고객별 첫 구매일
+select CustomerID,
+min(InvoiceDate) mndt
+from clothes.dataset3 
+group by 1;
+
+-- # 각 고객의 주문 일자, 구매액 조회
+select CustomerID,
+InvoiceDate, 
+UnitPrice * Quantity sales
+from clothes.dataset3;
+
+-- #첫 번째 구매했던 고객별 첫 구매월 테이블에 고객의 구매 내역 join
+select *
+from
+(select CustomerID,
+min(InvoiceDate) mndt
+from clothes.dataset3
+group by 1) a 
+left join
+(select CustomerID,
+InvoiceDate,
+UnitPrice * Quantity sales
+from clothes.dataset3) b 
+on a.CustomerID = b.CustomerID ; -- mndt : 각 고객의 최초 구매 일
+                                 -- datediff : 첫 구매 이후 몇 개월 뒤에 구매했는지
+select substr(mndt,1,7) mm,
+timestampdiff(month, mndt, InvoiceDate) datediff, -- timestamp: 시간 차이 계산 달 시 분 초 등
+count(distinct a.CustomerID) bu,
+sum(Sales) sales
+from
+(select CustomerID,
+min(InvoiceDate) mndt
+from clothes.dataset3
+group by 1) a
+left join
+(select customerid,
+InvoiceDate,
+UnitPrice * Quantity sales
+from clothes.dataset3) b
+on a.CustomerID= b.CustomerID 
+group by 1,2
